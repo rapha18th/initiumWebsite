e7a2f96bfdfd3371b63447aa253db0d79e619c26
rapha18th <rairorr@gmail.com>
2020-05-20 12:17:10 +0200

    about

--- src/components/about.js
+++ src/components/about.js
@@ -14,9 +14,9 @@ const Header = ({ data }) => (
         </div>
         <div className="left col-md-7 col-lg-8">
           <div className="about-details">
-            <span className="name">My Name is {data.name}.</span>
+            <span className="name"> {data.name}.</span>
             <h2 className="sub-position">
-              I'm a Professional {data.designation}.
+              {data.designation}.
             </h2>
             <div
               dangerouslySetInnerHTML={{
@@ -24,24 +24,6 @@ const Header = ({ data }) => (
               }}
             />
             <ul className="details">
-              <li>
-                <strong>Full Name</strong>
-                <p>{data.name}</p>
-              </li>
-              <li>
-                <strong>Age</strong>
-                <p>{data.age} Years</p>
-              </li>
-              <li>
-                <strong>Location</strong>
-                <p>{data.location}</p>
-              </li>
-              <li>
-                <strong>Email</strong>
-                <p>
-                  <a href={`mailto:${data.gmail}`}>{data.gmail}</a>
-                </p>
-              </li>
             </ul>
             <div className="socials">
               <ul>
